3311ddee557dc1a6d0e8a76e562da656e185279f
JamesVictor-o <victorjames40v@gmail.com>
2025-03-18 00:16:26 +0100

    ...

--- test/MarketPlace.t.sol
+++ test/MarketPlace.t.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {MarketPlace} from "../src/MarketPlace.sol";
 
 contract CounterTest is Test {
-    Counter public counter;
+    MarketPlace public counter;
 
     function setUp() public {
-        counter = new Counter();
+        counter = new MarketPlace();
         counter.setNumber(0);
     }
 
